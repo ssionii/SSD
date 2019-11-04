7518f2042c5c21e68d8836632de288271cb7b04e
seungmin97 <lsm0341@naver.com>
2019-11-04 15:24:46 +0900

    docs_title 수정

--- ssd_whale/web/js/docs.js
+++ ssd_whale/web/js/docs.js
@@ -1,10 +1,14 @@
-function chkword(obj, maxByte) {
-    var strValue = obj.value;
+var div_ = document.getElementById('docs_title');
+var todo_count = 0;
+
+div_.onkeyup = function () {
+    var strValue = div_.value;
     var strLen = strValue.length;
     var totalByte = 0;
     var len = 0;
     var oneChar = "";
     var str2 = "";
+    var maxByte = 10;
 
     for (var i = 0; i < strLen; i++){
         oneChar = strValue.charAt(i);
@@ -23,11 +27,9 @@ function chkword(obj, maxByte) {
         alert(maxByte + "자를 초과 입력 할 수 없습니다.");
         str2 = strValue.substr(0, len);
         obj.value = str2;
-        chkword(obj, 4000);
     }
 }
 
-var todo_count = 0;
 function toggle(id) {
     obj.document.getElementById(id);
 
