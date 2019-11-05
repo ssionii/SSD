b9d9ccfe0a2beeb0f5bc8a92adc084112619d79c
seungmin97 <lsm0341@naver.com>
2019-11-06 05:17:47 +0900

    seungmin2

--- ssd_whale/web/js/list.js
+++ ssd_whale/web/js/list.js
@@ -35,19 +35,59 @@ window.onload = function () {
     }catch (e) {
         alert(e.toString());
     }*/
-    list_count = 2;
+
+    list_count = 10;
     list = new Array(2);
     list[0] = new structlist();
     list[0].id = "599066f33c9405e4b1030dddf1bbbaaa4075";
     list[0].title = "아자아자";
     list[0].share = 0;
-    //alert(list[0].id + " " + list[0].title + " " + list[0].share);
 
     list[1] = new structlist();
     list[1].id = "225a921db007097b38080a7cd26bffc304e9";
     list[1].title = "안뇽";
     list[1].share = 1;
-    //alert(list[1].id + " " + list[1].title + " " + list[1].share);
+
+    list[2] = new structlist();
+    list[2].id = "599066f33c9405e4b1030dddf1bbbaaa4075";
+    list[2].title = "아자아자";
+    list[2].share = 0;
+
+    list[3] = new structlist();
+    list[3].id = "225a921db007097b38080a7cd26bffc304e9";
+    list[3].title = "안뇽";
+    list[3].share = 1;
+
+    list[4] = new structlist();
+    list[4].id = "599066f33c9405e4b1030dddf1bbbaaa4075";
+    list[4].title = "아자아자";
+    list[4].share = 0;
+
+    list[5] = new structlist();
+    list[5].id = "225a921db007097b38080a7cd26bffc304e9";
+    list[5].title = "안뇽";
+    list[5].share = 1;
+
+    list[6] = new structlist();
+    list[6].id = "225a921db007097b38080a7cd26bffc304e9";
+    list[6].title = "안뇽";
+    list[6].share = 1;
+
+
+    list[7] = new structlist();
+    list[7].id = "225a921db007097b38080a7cd26bffc304e9";
+    list[7].title = "안뇽";
+    list[7].share = 1;
+
+    list[8] = new structlist();
+    list[8].id = "225a921db007097b38080a7cd26bffc304e9";
+    list[8].title = "안뇽";
+    list[8].share = 1;
+
+    list[9] = new structlist();
+    list[9].id = "225a921db007097b38080a7cd26bffc304e9";
+    list[9].title = "안뇽";
+    list[9].share = 1;
 
     for (var i = 0; i < list_count; i++){
 
